ddfb5c0ff11660cdd01dbf29b7f8a419d5613e71
Ilya <domni3174@gmail.com>
2023-10-22 21:34:28 +0300

    Initial Commit

---
+++ ProductManager/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ProductManager
+//
+//  Created by Apple on 22.10.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ProductManager/ProductManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProductManagerApp.swift
+//  ProductManager
+//
+//  Created by Apple on 22.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct ProductManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
